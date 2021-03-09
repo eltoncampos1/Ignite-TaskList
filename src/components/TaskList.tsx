6611060fccfb7f26b7a720cec37094549e2664bb
elton de campos <eltoncampos36@gmail.com>
2021-03-09 18:20:13 -0300

    feat: create remove  task function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -34,6 +34,10 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const tasksFiltered = tasks.filter((task) => task.id !== id);
+
+    setTasks(tasksFiltered);
   }
 
   return (
